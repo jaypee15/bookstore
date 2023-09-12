d71367fb5889266f5a137632bc411e764f787d5f
jaypee15 <okoyejohnpaul15@gmail.com>
2023-09-12 21:04:49 +0100

    added registration, login and unit tests

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django Admin
     path("admin/", admin.site.urls),
+
+    # Local apps
     path("", include("pages.urls") ),
+    path("accounts/", include("accounts.urls")),
+
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    
+
 ]
