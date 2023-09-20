a18f65f8643e713baad21527ded8ff998b3ef00f
jaypee15 <okoyejohnpaul15@gmail.com>
2023-09-20 15:07:23 +0100

    added search functionality

--- books/views.py
+++ books/views.py
@@ -1,13 +1,32 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 from .models import Book
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = "books/book_list.html"
     context_object_name = "book_list"
+    login_url = "account_login"
 
-class BookDetailView(DetailView): 
+
+class BookDetailView(LoginRequiredMixin, DetailView): 
     model = Book
     template_name = "books/book_detail.html"
     context_object_name = "book"
+    login_url = "account_login"
+
+class SearchResultsListView(ListView): # new
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+    
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(Q(title__icontains=query) 
+                                   | Q(author__icontains=query)
+                                   )
+    
